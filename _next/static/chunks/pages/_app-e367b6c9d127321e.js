8180a5cfa7fd9417b758f28c73018b6cd18dcb05
jack <tathuan96@gmail.com>
2023-10-13 13:15:06 +0700

    feat: deploy resume as code

---
+++ _next/static/chunks/pages/_app-e367b6c9d127321e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{44234:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(14588)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(44234),_(66390)}),_N_E=n.O()}]);
